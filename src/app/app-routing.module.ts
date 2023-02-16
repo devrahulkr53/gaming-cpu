c65b1895d4b228c90d3447bb5731f91908a3fb9e
Rahul Kumar <isrkrahul53@gmail.com>
2023-02-16 14:29:47 +0530

    terms and conditions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'about-us', loadChildren: () => import('./components/about-us/about-us.module').then(m=>m.AboutUsModule) },
   { path: 'contact-us', loadChildren: () => import('./components/contact-us/contact-us.module').then(m=>m.ContactUsModule) },
   { path: 'privacy-policy', loadChildren: () => import('./components/privacy-policy/privacy-policy.module').then(m=>m.PrivacyPolicyModule) },
+  { path: 'terms-conditions', loadChildren: () => import('./components/terms-conditions/terms-conditions.module').then(m=>m.TermsConditionsModule) },
   { path: 'gaming-pc', loadChildren: () => import('./components/gaming-pc/gaming-pc.module').then(m=>m.GamingPcModule) },
   { path: 'product', loadChildren: () => import('./components/product/product.module').then(m=>m.ProductModule) },
   { path: 'checkout', loadChildren: () => import('./components/checkout/checkout.module').then(m=>m.CheckoutModule) },
